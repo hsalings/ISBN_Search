32ca2f19b88ca1062efb2195abbd4f43b22fd2aa
Hanna Salings <hsalings@hotmail.com>
2020-09-27 21:28:59 -0500

    Update isbnSearch.py

--- isbnSearch.py
+++ isbnSearch.py
@@ -2,22 +2,26 @@ import isbnlib
 import pandas as pd
 
 def get_isbn_metadata(isbn, isbnList):
+  #Takes the ISBN and appends information from it into an array
   try:
-    isbnList.append(isbnlib.meta(str(isbn), service='wiki'))
-    isbnList.append(isbnlib.cover(str(isbn)))
-    return isbnList
+    isbnList.append(isbnlib.meta(str(isbn), service='wiki')) #Appends ISBN number, title, author, publisher, year, and language
+    isbnList.append(isbnlib.cover(str(isbn))) #Appends link to cover of book
+    return isbnList #Returns the appended array
 
   except isbnlib.ISBNLibException:
+    #Try different service (Google Books) for searching the ISBN information
   	isbnList.append(isbnlib.meta(str(isbn), service='goob'))
   	isbnList.append(isbnlib.cover(str(isbn)))
   	return isbnList
 
   else:
+    # Try different service (OpenL) for searching the ISBN information
   	isbnList.append(isbnlib.meta(str(isbn), service='openl'))
   	isbnList.append(isbnlib.cover(str(isbn)))
   	return isbnList
 
 def toCSVFile(isbnList):
+  #Convert the array into a CSV file format
   df = pd.DataFrame(isbnList)
   return df.to_csv('test.csv', index=False, encoding='utf-8 ')
 
