61b6bffc157fc268f5a37bf27eb06c6fcec69c90
Hanna Salings <hsalings@hotmail.com>
2020-10-02 16:04:28 -0500

    Update isbnSearch.py

--- isbnSearch.py
+++ isbnSearch.py
@@ -11,6 +11,9 @@ import isbnlib  # For searching ISBNs
 import pandas as pd  # For the dataframe to convert to .CSV
 import PySimpleGUI as sg  # For the GUI
 import csv
+import xlrd
+import openpyxl
+from openpyxl.utils.dataframe import dataframe_to_rows
 
 # Functions
 def tupleData(isbn, data, quan, cond, pallet, lot):
@@ -24,7 +27,7 @@ def tupleData(isbn, data, quan, cond, pallet, lot):
     tempList.append(cond) # Add the condition of the book
     tempList.append(lot) # Add the lot number
     tempList.append(pallet) # Add the pallet number
-    tempList.append(isbnlib.cover(isbn))  # Add cover of ISBN
+    #tempList.append(isbnlib.cover(isbn))  # Add cover of ISBN
 
     temp = tuple(tempList)
     return temp
@@ -43,10 +46,88 @@ def get_isbn_metadata(isbn):
 
         return isbnMeta
 
-def toCSVFile(file_name, isbnList):
+
+def append_df_to_excel(filename, df, sheet_name='Sheet1', startrow=None,
+                       truncate_sheet=False,
+                       **to_excel_kwargs):
+    """
+    Append a DataFrame [df] to existing Excel file [filename]
+    into [sheet_name] Sheet.
+    If [filename] doesn't exist, then this function will create it.
+
+    Parameters:
+      filename : File path or existing ExcelWriter
+                 (Example: '/path/to/file.xlsx')
+      df : dataframe to save to workbook
+      sheet_name : Name of sheet which will contain DataFrame.
+                   (default: 'Sheet1')
+      startrow : upper left cell row to dump data frame.
+                 Per default (startrow=None) calculate the last row
+                 in the existing DF and write to the next row...
+      truncate_sheet : truncate (remove and recreate) [sheet_name]
+                       before writing DataFrame to Excel file
+      to_excel_kwargs : arguments which will be passed to `DataFrame.to_excel()`
+                        [can be dictionary]
+
+    Returns: None
+
+    (c) [MaxU](https://stackoverflow.com/users/5741205/maxu?tab=profile)
+    """
+    from openpyxl import load_workbook
+
+    # ignore [engine] parameter if it was passed
+    if 'engine' in to_excel_kwargs:
+        to_excel_kwargs.pop('engine')
+
+    writer = pd.ExcelWriter(filename, engine='openpyxl')
+
+    # Python 2.x: define [FileNotFoundError] exception if it doesn't exist
+    try:
+        FileNotFoundError
+    except NameError:
+        FileNotFoundError = IOError
+
+    try:
+        # try to open an existing workbook
+        writer.book = load_workbook(filename)
+
+        # get the last row in the existing Excel sheet
+        # if it was not specified explicitly
+        if startrow is None and sheet_name in writer.book.sheetnames:
+            startrow = writer.book[sheet_name].max_row
+
+        # truncate sheet
+        if truncate_sheet and sheet_name in writer.book.sheetnames:
+            # index of [sheet_name] sheet
+            idx = writer.book.sheetnames.index(sheet_name)
+            # remove [sheet_name]
+            writer.book.remove(writer.book.worksheets[idx])
+            # create an empty sheet [sheet_name] using old index
+            writer.book.create_sheet(sheet_name, idx)
+
+        # copy existing sheets
+        writer.sheets = {ws.title: ws for ws in writer.book.worksheets}
+    except FileNotFoundError:
+        # file does not exist yet, we will create it
+        pass
+
+    if startrow is None:
+        startrow = 0
+
+    # write out the new sheet
+    df.to_excel(writer, sheet_name, header=False, index=False, startrow=startrow, **to_excel_kwargs)
+
+    # save the workbook
+    writer.save()
+
+
+def toCSVFile(filename, isbnList):
     # Convert the list into a CSV file format
     df = pd.DataFrame(isbnList)
-    df.to_csv(file_name, mode='a', index=False, header=False, encoding='utf-8 ')
+    #df.to_csv(filename, mode='a', index=False, header=False, encoding='utf-8 ')
+
+    append_df_to_excel(filename, df)
+
     return sg.popup('ISBN has been added to Inventory File')
 
 def make_window(isbn): #Layouts cannot be re-used; must create a new window with a function
@@ -74,11 +155,9 @@ def make_multi_window():
     return sg.Window('ISBN Already Exists', layout_multi)
 
 def make_edit_window(toEdit):
-    radio = str(toEdit).strip('[]')
     layout_edit = [
         [sg.Text('Locations of ISBN in Inventory:')],
         [sg.Text('Select which item to edit')],
-        #[sg.Radio('Item: ', radio)],
         [sg.Listbox(values=toEdit, size=(130,6), key='-LIST-', bind_return_key=True)],
         [sg.Button('Submit'), sg.Button('Cancel')]
     ]
@@ -90,15 +169,76 @@ def make_edit_window(toEdit):
             editWindow.Close()
             break
         if eventEdit == 'Submit':
-            choice = valuesEdit['-LIST-']
-            make_choice_window(choice)
+            if valuesEdit['-LIST-']:
+                choice = valuesEdit['-LIST-']
+                update_CSV(filename, choice)
+    return
+
+def update_CSV(filename, toUpdate):
+    file = openpyxl.load_workbook(filename)
+    sheets = file.sheetnames
+    #For reference, toUpdate[0][1] is equivalent to the title in the list
+    rowNumber = ''
+    isbn = False
+    title = False
+    quan = False
+    cond = False
+    lot = False
+    pall = False
+
+    for sheet in sheets:
+        currentSheet = file[sheet]
+
+        for row in range(1, currentSheet.max_row + 1):
+            for column in "ABCDEFGHIJK":
+
+                cell_name = "{}{}".format(column, row)
+                #print("cell position {} has value {}".format(cell_name, currentSheet[cell_name].value))
+                if currentSheet[cell_name].value == toUpdate[0][0]:
+                    isbn = True
+                    isbnOfEdit = cell_name
+
+                if currentSheet[cell_name].value == toUpdate[0][1]:
+                    title = True
+                    titleOfEdit = cell_name
+
+                if currentSheet[cell_name].value == toUpdate[0][7]:
+                    quan = True
+                    quanOfEdit = cell_name
+
+                if currentSheet[cell_name].value == toUpdate[0][8]:
+                    cond = True
+                    condOfEdit = cell_name
+
+                if currentSheet[cell_name].value == toUpdate[0][9]:
+                    lot = True
+                    lotOfEdit = cell_name
+
+                if currentSheet[cell_name].value == toUpdate[0][10]:
+                    pall = True
+                    pallOfEdit = cell_name
+
+            if isbn == True and title == True and quan == True and cond == True and lot == True and pall == True:
+                #print('test')
+                #print(cell_name)
+                #print(isbnOfEdit + ' ' + titleOfEdit + ' ' +  quanOfEdit + ' ' +  condOfEdit + ' ' +  lotOfEdit + ' ' + pallOfEdit)
+                rowNumber = cell_name[1]
+
+                # Set back to False since it is still looping through all instances in the file
+                isbn = False
+                title = False
+                quan = False
+                cond = False
+                lot = False
+                pall = False
+
+                for Q in range(1, currentSheet.max_row + 1):
+                    for col in rowNumber:
+                        print(currentSheet[cell_name].value)
+
     return
 '''
-def update_CSV(filename, toUpdate):
-    
-'''
-'''
-def make_choice_window(choice):
+def make_change_window(choice):
     layout_choice = [
         []
     ]
@@ -137,7 +277,8 @@ def display_CSV(filename):
     header_list = []
 
     try:
-        df = pd.read_csv(filename, sep=',', engine='python', header=None)
+        #df = pd.read_csv(filename, sep=',', engine='python', header=None)
+        df = pd.read_excel(filename, header=None)
         header_list = df.iloc[0].tolist()
         data = df[1:].values.tolist()
     except:
@@ -145,15 +286,16 @@ def display_CSV(filename):
         return
 
     displayLayout = [
-        [sg.Table(values=data, headings=header_list, display_row_numbers=True, auto_size_columns=False, num_rows=min(25, len(data)))]
+        [sg.Table(values=data, headings=header_list, display_row_numbers=True, num_rows=min(25, len(data)))]
     ]
     displayWindow = sg.Window('Inventory', displayLayout, grab_anywhere=False)
     disEvent, disValues = displayWindow.read()
     displayWindow.close()
 
 windowMultInst_active = False # Second window for checking for multiple instances of an entered ISBN
-condition = ' '
-filename = 'test.csv'
+condition = '-'
+filename = 'test.xlsx'
+
 isbn = enterISBN(filename)
 window = make_window(isbn)
 
@@ -205,6 +347,8 @@ while True:  # Loop for window to remain open
             pallet = '-'
         if lot == 'LOT ':
             lot = '-'
+        if quantity == '':
+            quantity = '-'
 
         if not isbnlib.notisbn(isbn, level='strict'): #Verify the ISBN is valid
             metadataInfo = get_isbn_metadata(isbn)
@@ -212,18 +356,28 @@ while True:  # Loop for window to remain open
             isbnList.append(tupleToList)
 
             alreadyExist = False # For testing if the ISBN is already in the CSV file
-            test = []
-            with open(filename) as f:
-                fileSum = sum(1 for line in f)
 
+            book = xlrd.open_workbook(filename)
+            toEdit = []
+            test = []
+            for sheet in book.sheets():
+                for rowid in range(sheet.nrows):
+                    row = sheet.row(rowid)
+                    for colid, cell in enumerate(row):
+                        if cell.value == isbn:
+                            alreadyExist = True
+                            toEdit.append(sheet.row_values(rowid, start_colx=0))
+            '''
             with open(filename, 'rt') as f:
-                reader = csv.reader(f, delimiter=',')
+                #reader = csv.reader(f, delimiter=',')
+                reader = xlrd.open_workbook(filename)
                 toEdit = []
                 for col in reader:
                     for row in col:
                         if row == isbn:
                             alreadyExist = True
                             toEdit.append(col)
+            '''
 
             if alreadyExist:
                 windowMultInst_active = True
